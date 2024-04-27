8e0910e12068aca27c4dd2f69a3bae01753e54b8
markklimenko <mark.klimenko.w@gmail.com>
2024-04-27 02:36:28 -0600

    Add

---
+++ code/kafka_java_example/src/main/kotlin/com/example/java_example/JavaExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.java_example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<JavaExampleApplication>(*args)
+}
---
+++ code/kafka_java_example/src/test/kotlin/com/example/java_example/JavaExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.java_example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JavaExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
