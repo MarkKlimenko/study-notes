954c5941bd9f6c646b7860c21e8e26fe9dfabe75
markklimenko <mark.klimenko.w@gmail.com>
2024-04-27 02:43:50 -0600

    Add

--- code/kafka_java_example/src/main/kotlin/com/example/java_example/Application.kt
+++ code/kafka_java_example/src/main/kotlin/com/example/java_example/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class JavaExampleApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<JavaExampleApplication>(*args)
+    runApplication<Application>(*args)
 }
