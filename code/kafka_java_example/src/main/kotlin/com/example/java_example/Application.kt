b87e7e1727ae4cd617512eda310e756d2e233390
markklimenko <mark.klimenko.w@gmail.com>
2024-04-30 14:36:45 -0600

    Add

--- code/kafka_java_example/src/main/kotlin/com/example/java_example/Application.kt
+++ code/kafka_java_example/src/main/kotlin/com/example/java_example/Application.kt
@@ -3,14 +3,32 @@ package com.example.java_example
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
 import kotlinx.coroutines.withContext
 
 class Application
 
 fun main(args: Array<String>) = runBlocking<Unit> {
-    withContext(Dispatchers.Default) {
-        //async { KafkaExample().startKafkaProducerWithoutId() }
-        launch { KafkaExample().startKafkaProducerWithId() }
-        launch { KafkaExample().startKafkaConsumers() }
+    //withContext(Dispatchers.Default) {
+    //    //async { KafkaExample().startKafkaProducerWithoutId() }
+    //    launch { KafkaExample().startKafkaProducerWithId() }
+    //    launch { KafkaExample().startKafkaConsumers() }
+    //}
+
+    //withContext(Dispatchers.Default) {
+        doWork()
+    //}
+}
+
+suspend fun doWork() {
+    supervisorScope {
+        launch {
+            println("BLAAAAAA")
+            Thread.sleep(50000)
+        }
+
+        launch {
+            println("AAAAAABL")
+        }
     }
 }
